6a432267c2b7a836492ef99ac01ee017356f2975
Dagostini01 <pedro01dagostini@gmail.com>
2026-03-04 17:42:27 -0300

    chore: add fastify swagger generation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
 import Fastify from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { serializerCompiler,validatorCompiler } from "fastify-type-provider-zod";
+import {
+  jsonSchemaTransform,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
 import z from "zod";
+
 const app = Fastify({
   logger: true,
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Bootcamp Treinos API",
+      description: "API para bootcamp de treinos de FSC",
+      version: "1.0.0",
+    },
+    servers: [{
+        description: "localhost",
+        url: "http://localhost:8081",
+    }],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
 });
 
 app.withTypeProvider<ZodTypeProvider>().route({
@@ -11,12 +40,12 @@ app.withTypeProvider<ZodTypeProvider>().route({
   schema: {
     response: {
       200: z.object({
-        hello: z.string(),
+        message: z.string(),
       }),
     },
   },
   handler: () => {
-    return { hello: "world" };
+    return { message: "world" };
   },
 });
 
@@ -26,3 +55,4 @@ try {
   app.log.error(err);
   process.exit(1);
 }
+
