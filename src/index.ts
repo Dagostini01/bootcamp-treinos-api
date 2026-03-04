4bc66a8a5b240eaca0c5b85b513d9207fad0ad7c
Dagostini01 <pedro01dagostini@gmail.com>
2026-03-04 17:17:23 -0300

    chore: add zod and fastify-type-provider-zod setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import Fastify from "fastify";
-const fastify = Fastify({
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+const app = Fastify({
   logger: true,
 });
 
-fastify.get("/", () => {
-  return { hello: "world" };
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/",
+  schema: {
+    response: {
+      200: z.object({
+        hello: z.string(),
+      }),
+    },
+  },
+  handler: () => {
+    return { hello: "world" };
+  },
 });
 
 try {
-  await fastify.listen({ port: Number(process.env.PORT) || 8081 });
+  await app.listen({ port: Number(process.env.PORT) || 8081 });
 } catch (err) {
-  fastify.log.error(err);
+  app.log.error(err);
   process.exit(1);
 }
